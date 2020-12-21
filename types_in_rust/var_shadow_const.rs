0a2ccb19eab4118aba6f68c127bc8ac02fae1e9f
Sathishvinayk <sathishvinayk@gmail.com>
2020-12-21 11:25:39 +0530

    added_var_shadow

---
+++ types_in_rust/var_shadow_const.rs
@@ -0,0 +1,15 @@
+fn main(){
+    let mut x:i8 = 1;
+    // let t&y: bool = true; 
+    println!("X before {}",x);
+
+    let ac_a = 512;
+
+    x = 2;
+
+    let ac_a = ac_a + 12;
+
+    const COLOR: &str = "Red";
+
+    println!("Values of x, ac_a and color is {}, {}, {}", x, ac_a, COLOR);
+}
